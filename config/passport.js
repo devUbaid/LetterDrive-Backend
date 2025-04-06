b2f1420522ff8c2499e6e69d6393878d4d90ab0d
devUbaid <devubaid493@gmail.com>
2025-04-06 07:34:01 +0530

    new update

--- config/passport.js
+++ config/passport.js
@@ -1,26 +1,26 @@
-const passport = require("passport")
-const GoogleStrategy = require("passport-google-oauth20").Strategy
-const User = require("../models/User")
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
 
 passport.serializeUser((user, done) => {
-  done(null, user.id)
-})
+  done(null, user.id);
+});
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await User.findById(id)
-    done(null, user)
+    const user = await User.findById(id);
+    done(null, user);
   } catch (error) {
-    done(error, null)
+    done(error, null);
   }
-})
+});
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: `${process.env.SERVER_URL}/api/auth/google/callback`,
       scope: [
         "profile",
         "email",
@@ -30,15 +30,15 @@ passport.use(
     async (accessToken, refreshToken, profile, done) => {
       try {
         // Check if user already exists
-        let user = await User.findOne({ googleId: profile.id })
-        const isNewUser = !user
+        let user = await User.findOne({ googleId: profile.id });
+        const isNewUser = !user;
 
         if (user) {
           // Update tokens for existing user (login flow)
-          user.googleAccessToken = accessToken
-          user.googleRefreshToken = refreshToken
-          await user.save()
-          return done(null, user)
+          user.googleAccessToken = accessToken;
+          user.googleRefreshToken = refreshToken;
+          await user.save();
+          return done(null, user);
         }
 
         // Create new user (signup flow)
@@ -49,14 +49,13 @@ passport.use(
           picture: profile.photos[0].value,
           googleAccessToken: accessToken,
           googleRefreshToken: refreshToken,
-        })
+        });
 
-        await user.save()
-        return done(null, user)
+        await user.save();
+        return done(null, user);
       } catch (error) {
-        return done(error, null)
+        return done(error, null);
       }
-    },
-  ),
-)
-
+    }
+  )
+);
